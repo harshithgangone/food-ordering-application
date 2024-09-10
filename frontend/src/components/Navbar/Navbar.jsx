21152d7ed88087bbcd5e1f27b2737d618f7d8e0c
harshithgangone <harshith.gangone@gmail.com>
2024-09-10 22:52:14 +0530

    added headder and items categories

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./Navbar.css"
 import { assets } from '../../assets/assets'
 const navbar = () => {
-    const[menu,setMenu]=useState("menu");
+    const[menu,setMenu]=useState("home");
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className='logo' />
