97357fdaf2418cd630f99ecdcaacc6c03f372ffd
harshithgangone <harshith.gangone@gmail.com>
2024-09-12 20:43:11 +0530

    made all components in home page responsive

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
        <div className="header-contents">
-        <h2>Order your favirite food here</h2>
+        <h2>Order your favorite food here</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa nulla sed consectetur reprehenderit assumenda corporis amet vero autem quo. Enim odio, dignissimos temporibus consequuntur ipsum id veritatis obcaecati atque porro nesciunt deserunt! Enim inventore asperiores, cumque nulla similique quam, illo, iure est explicabo debitis excepturi!</p>
         <button>View Menu</button>
        </div>
